c3d6ad2028c22af05a35496d6993a7fd3100f0d0
Roman <romanlee1116@gmail.com>
2024-07-08 00:38:16 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ad-demo/',
-  plugins: [vue()],
-  build: {
-    outDir: 'docs',
-  },
+    base: '/neon-hong-kong/',
+    plugins: [vue()],
+    build: {
+		outDir: 'docs',
+    },
 })
